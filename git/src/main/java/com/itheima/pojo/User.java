7c1fdef341d63374f6de29405f26cacd8d787168
maodenghui <943499015@qq.cmo>
2019-07-18 10:35:15 +0800

    firstcommit

---
+++ git/src/main/java/com/itheima/pojo/User.java
@@ -0,0 +1,37 @@
+package com.itheima.pojo;
+
+/**
+ * @ClassName User
+ * @Description TODO:
+ * @Author 阿毛
+ * @Date 10:29 2019/7/18
+ * @Version 2.1
+ **/
+public class User {
+    private String name;
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                '}';
+    }
+}
