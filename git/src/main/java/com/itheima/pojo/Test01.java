f015aac965814744d4c45c638b803bac9912b025
“‘ <943499015@qq.com>
2024-03-23 22:34:28 +0800

    aizjiasf ajsdjf alsjiewn

--- git/src/main/java/com/itheima/pojo/Test01.java
+++ git/src/main/java/com/itheima/pojo/Test01.java
@@ -3,7 +3,10 @@ package main.java.com.itheima.pojo;
 public class Test01 {
 
     public static void main(String[] args) {
-        System.out.println("hahah");
+        System.out.println("hahah");// hkjjhkj
+
+
+
     }
 
 }
