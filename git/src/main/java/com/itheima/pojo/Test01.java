e50f42263b1389f290f7471b8213a7de8a9853bd
“‘ <943499015@qq.com>
2024-03-24 11:31:39 +0800

    commit info

--- git/src/main/java/com/itheima/pojo/Test01.java
+++ git/src/main/java/com/itheima/pojo/Test01.java
@@ -3,7 +3,7 @@ package main.java.com.itheima.pojo;
 public class Test01 {
 
     public static void main(String[] args) {
-        System.out.println("hahah");// hkjjhkj
+        System.out.println("hahah");// hkjjhkj               sdfasdf asdf
 
 
 
